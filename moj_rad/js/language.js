63a7d6ddd1f7d9828f94b81da8362f060f24aae8
Luka Matovic <ml170722d@student.etf.bg.ac.rs>
2020-06-27 23:39:44 +0200

    new web pages added

---
+++ moj_rad/js/language.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {//rs click listener
+    $('#rs').click(function () {
+        alert("active");
+    });
+
+    //en click listener
+    $('#en').click(function () {
+        alert("active");
+    });
+});
