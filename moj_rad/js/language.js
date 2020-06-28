fd5e399e75cc923eced078a185c810af9c31d08e
Luka Matovic <ml170722d@student.etf.bg.ac.rs>
2020-06-28 16:51:12 +0200

    created tameplate for translation

--- moj_rad/js/language.js
+++ moj_rad/js/language.js
@@ -1,10 +1,99 @@
-$(document).ready(function () {//rs click listener
+var language;
+
+$(document).ready(function () {
+    //rs click listener
     $('#rs').click(function () {
-        alert("active");
+        setLanguage('rs');
+        translate();
+
     });
 
     //en click listener
     $('#en').click(function () {
-        alert("active");
+        setLanguage('en');
+        translate();
     });
+
+
+
 });
+
+function getLanguage() {
+    (localStorage.getItem('language') == null) ? setLanguage('rs') : false;
+    $.ajax({
+        url: '../js/language/' + localStorage.getItem('language') + '.json',
+        dataType: 'json', async: false, dataType: 'json',
+        success: function (lang) { language = lang }
+    });
+}
+
+function setLanguage(lang) {
+    localStorage.setItem('language', lang);
+}
+
+function translateNavBar() {
+    let i = 0;
+    $('#navbarNavDropdown').find('ul').find('li').find('a').each(function () {
+        switch (i++) {
+            case 0:
+                $(this).text(language.nav_bar_home);
+                break;
+            case 1:
+                $(this).text(language.nav_bar_service_dropdown);
+                break;
+            case 2:
+                $(this).text(language.nav_bar_service_item1);
+                break;
+            case 3:
+                $(this).text(language.nav_bar_service_item2);
+                break;
+            case 4:
+                $(this).text(language.nav_bar_service_item3);
+                break;
+            case 5:
+                $(this).text(language.nav_bar_shedule);
+                break;
+            case 6:
+                $(this).text(language.nav_bar_my_acc);
+                break;
+            case 7:
+                $(this).text(language.nav_bar_about_us);
+                break;
+            default:
+                alert('error');
+                break;
+        }
+    });
+}
+
+function translateFooter() {
+    let i = 0;
+    $('footer').find('p').find('span').each(function () {
+        switch (i++) {
+            case 0:
+                $(this).text(language.credentials_1st);
+                break;
+            case 1:
+                $(this).text(language.credentials_2nd);
+                break;
+            case 2:
+                $(this).text(language.credentials_3rd);
+                break;
+            case 3:
+                $(this).text(language.credentials_4th);
+                break;
+            default:
+                alert('error');
+                break;
+        }
+    })
+}
+
+function translate() {
+    getLanguage();
+    translateNavBar();
+    translateFooter();
+    
+    //user defined functin
+    translatePage(language);
+}
