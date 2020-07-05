d0f4c4711f7e002d35b7db6a7eae4605ab7a09ca
Luka Matovic <ml170722d@student.etf.bg.ac.rs>
2020-07-06 00:58:51 +0200

    potencijalni problem (merge-ovanje)

--- moj_rad/js/injectListElem.js
+++ moj_rad/js/injectListElem.js
@@ -1,40 +1,38 @@
-function loadListItems(callback){
+function loadListItems(callback) {
     $('main li').each(function () {
         $(this).load('./listItem.html', function () {
 
-            insertStarsToItem($(this).find('div[name$="stars"]'));
+            insertStarsToItem($(this).find('div[name$="stars"]'), function () {
+                let i = 0;
+                $('.item form').each(function () {
+                    $(this).find('textarea').attr("name", "txta_" + i);
+                    $(this).find('input').attr("name", "txta_" + i);
+                    i++;
 
-            let i = 0;
-            $('.item form').each(function () {
-                $(this).find('textarea').attr("name", "txta_" + i);
-                $(this).find('input').attr("name", "txta_" + i);
-                i++;
+                    $(this).find('input').click(function (e) {
+                        let id = $(this).attr("name").split("_")[1];
+                        let commnetText = $('textarea[name$="' + $(this).attr("name") + '"]').val();
+                        if (commnetText == "") return;
+                        $('textarea[name$="' + $(this).attr("name") + '"]').val("");
 
-                $(this).find('input').click(function (e) {
-                    let id = $(this).attr("name").split("_")[1];
-                    let commnetText = $('textarea[name$="' + $(this).attr("name") + '"]').val();
-                    if (commnetText == "") return;
-                    $('textarea[name$="' + $(this).attr("name") + '"]').val("");
-
-                    let comment = $('<label></label>').text(commnetText);
-                    $(comment).addClass("col-sm-12");
-                    $("#comment-container-" + id).append(comment);
+                        let comment = $('<label></label>').text(commnetText);
+                        $(comment).addClass("col-sm-12");
+                        $("#comment-container-" + id).append(comment);
 
 
+                    });
                 });
+
+                i = 0;
+                $('.comment-container').each(function () {
+                    $(this).attr("id", "comment-container-" + i++);
+                });
+
+                
+                callback();
             });
-
-            i = 0;
-            $('.comment-container').each(function () {
-                $(this).attr("id", "comment-container-" + i++);
-            });
-
-            setMediaFor('iframe', 2, 'yoga');
-            setMediaFor('img', 2, 'yoga');
-
-            callback(this);
-            
+            setMediaFor(this);
         });
     });
-    
+
 }
--- moj_rad/js/language.js
+++ moj_rad/js/language.js
@@ -1,17 +1,20 @@
 var language;
 
-$().ready(function () {
+function getHeaderAndFooter(callback) {
     //load header and set listeners
     $('header').load('./header.html', function () {
         setLanguageListeners();
-
-        translate();
         setCurMaker();
     });
     
     //load footer
-    $('footer').load('./footer.html');
-});
+    $('footer').load('./footer.html', function(){
+        translate();
+        if(callback != undefined){
+            callback();
+        }
+    });
+};
 
 function setCurMaker() {
     let url = $(location).attr('pathname').split('/');
